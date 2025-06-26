0ce7927050798d58ff0fa412a04d4ba8e3c8778e
Arun Samy <134082910+arunsamy4444@users.noreply.github.com>
2025-06-26 11:56:34 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -65,7 +65,7 @@ export default function App() {
 
   const handleReminderSubmit = async (formData) => {
     try {
-      const res = await fetch("http://localhost:5000/set-reminder", {
+      const res = await fetch(`${process.env.REACT_APP_BASE_URL}/set-reminder`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
